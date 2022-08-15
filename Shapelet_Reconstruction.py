0b2247c962d4f3861e5011ff09c553b48a712886
tojoub <63126897+tojoub@users.noreply.github.com>
2022-08-15 12:11:53 +0200

    Update Shapelet_Reconstruction.py

--- Shapelet_Reconstruction.py
+++ Shapelet_Reconstruction.py
@@ -1,74 +1,145 @@
 from import_and_installations import *
-from Dataset_generation import n,dataset,labelset
+from Dataset_generation import generating_dataset,x,y,numPix,deltaPix,data_class, psf_class, kwargs_numerics, exp_time, background_rms
 
-def shapelet_reconstruction(n,choice_type_shapelet,dataset,beta,nmax,x,y,deltaPix):
-  
-  
-  dataset_reconstructed=[]
-  coeff_dataset=[]
 
-  ## Choose the type of shapelets : Polar or Cartesian shapelets
-  shapeletSet = choice_type_shapelet
-  for ind in tqdm(range(n)):
-    coeff_dataset_local=[]
-    dataset_reconstructed_local=[]
+def shapelet_reconstruction(n,source_dataset,source_reconstruction,choice_type_shapelet,beta,nmax,x,y,deltaPix):
+  variable_dataset=False
     
-    image_1d = util.image2array(dataset[ind][0])
-    coeff_ngc = shapeletSet.decomposition(image_1d, x, y, nmax, beta, deltaPix=1, center_x=0, center_y=0) 
-    # reconstruct NGC1300 with the shapelet coefficients
-    image_reconstructed = shapeletSet.function(x, y, coeff_ngc, nmax, beta, center_x=0, center_y=0)
-    # turn 1d array back into 2d image
-    image_reconstructed_2d = util.array2image(image_reconstructed)  # map 1d data vector in 2d image
-    dataset_reconstructed_local.append(image_reconstructed_2d)
-    coeff_dataset_local.append(coeff_ngc)
+  if not os.path.exists(source_reconstruction):
+    os.makedirs(source_reconstruction)
+    os.makedirs(source_reconstruction+'/Lensed_Dataset')
+    os.makedirs(source_reconstruction+'/Unlensed_Dataset')
+    os.makedirs(source_reconstruction+'/Noisy_Dataset')
+    
+    os.makedirs(source_reconstruction+'/DataFrame_unlensed_coeff_dataset')
+    os.makedirs(source_reconstruction+'/DataFrame_lensed_coeff_dataset')
+    os.makedirs(source_reconstruction+'/DataFrame_noisy_coeff_dataset')
+    
+    variable_dataset=True
+    
+  if variable_dataset:
+      
+      unlensed_coeff_dataset=[]
+      lensed_coeff_dataset=[]
+      noisy_coeff_dataset=[]
+     
+      ## Choose the type of shapelets : Polar or Cartesian shapelets
+      shapeletSet = choice_type_shapelet
+      
+       
+      for i in tqdm(range(n)):
+          
+        ##Unlensed data reconstruction
+        folder_temp=source_dataset+'/Unlensed_Dataset/'    
+        image = matplotlib.image.imread(folder_temp + str(i)+'.png')
+        image=image[:,:,0:3]
+        R=image[:,:,0]
+        G=image[:,:,1]
+        B=image[:,:,2]
+        unlensed_data= 0.2125* R + 0.7154* G + 0.0721 *B
+        
+        image_1d = util.image2array(unlensed_data)
+        coeff_ngc = shapeletSet.decomposition(image_1d, x, y, nmax, beta, deltaPix=deltaPix, center_x=0, center_y=0) 
+        # reconstruct the image with the shapelet coefficients
+        image_reconstructed = shapeletSet.function(x, y, coeff_ngc, nmax, beta, center_x=0, center_y=0)
+        # turn 1d array back into 2d image
+        image_reconstructed_2d = util.array2image(image_reconstructed)  # map 1d data vector in 2d image
+        image_reconstructed_2d=image_reconstructed_2d/np.max(image_reconstructed_2d)
+        # save in the wanted folder
+        matplotlib.image.imsave(source_reconstruction+'/Unlensed_Dataset/'+str(i)+'.png',image_reconstructed_2d,vmin=np.min(image_reconstructed_2d),vmax=np.max(image_reconstructed_2d))
+        unlensed_coeff_dataset.append(np.array(coeff_ngc))
+        
+        ##Lensed data reconstruction
+        folder_temp=source_dataset+'/Lensed_Dataset/'    
+        image = matplotlib.image.imread(folder_temp + str(i)+'.png')
+        image=image[:,:,0:3]
+        R=image[:,:,0]
+        G=image[:,:,1]
+        B=image[:,:,2]
+        lensed_data= 0.2125* R + 0.7154* G + 0.0721 *B
+        
+        image_1d = util.image2array(lensed_data)  
+        # decompose image and return the shapelet coefficients
+        coeff_ngc = shapeletSet.decomposition(image_1d, x, y, nmax, beta, deltaPix=deltaPix, center_x=0, center_y=0) 
+        # reconstruct the image with the shapelet coefficients
+        image_reconstructed = shapeletSet.function(x, y, coeff_ngc, nmax, beta, center_x=0, center_y=0)
+        # turn 1d array back into 2d image
+        image_reconstructed_2d = util.array2image(image_reconstructed)  # map 1d data vector in 2d image
+        image_reconstructed_2d=image_reconstructed_2d/np.max(image_reconstructed_2d)
+        # save in the wanted folder
+        matplotlib.image.imsave(source_reconstruction+'/Lensed_Dataset/'+str(i)+'.png',image_reconstructed_2d,vmin=np.min(image_reconstructed_2d),vmax=np.max(image_reconstructed_2d))
+        lensed_coeff_dataset.append(np.array(coeff_ngc))
+        
+        ##Noisy data reconstruction
+        folder_temp=source_dataset+'/Noisy_Dataset/'    
+        image = matplotlib.image.imread(folder_temp + str(i)+'.png')
+        image=image[:,:,0:3]
+        R=image[:,:,0]
+        G=image[:,:,1]
+        B=image[:,:,2]
+        noisy_data= 0.2125* R + 0.7154* G + 0.0721 *B
+        
+        image_1d = util.image2array(noisy_data)  
+        # decompose image and return the shapelet coefficients
+        coeff_ngc = shapeletSet.decomposition(image_1d, x, y, nmax, beta, deltaPix=deltaPix, center_x=0, center_y=0) 
+        # reconstruct the image with the shapelet coefficients
+        image_reconstructed = shapeletSet.function(x, y, coeff_ngc, nmax, beta, center_x=0, center_y=0)
+        # turn 1d array back into 2d image
+        image_reconstructed_2d = util.array2image(image_reconstructed)  # map 1d data vector in 2d image
+        image_reconstructed_2d=image_reconstructed_2d/np.max(image_reconstructed_2d)
+        # save in the wanted folder
+        matplotlib.image.imsave(source_reconstruction+'/Noisy_Dataset/'+str(i)+'.png',image_reconstructed_2d,vmin=np.min(image_reconstructed_2d),vmax=np.max(image_reconstructed_2d))
+        noisy_coeff_dataset.append(np.array(coeff_ngc))
+        
+        
+      #unlensed_dataset_reconstructed=np.array(unlensed_dataset_reconstructed)
+      unlensed_coeff_dataset=np.array(unlensed_coeff_dataset)
+    
+      #lensed_dataset_reconstructed=np.array(lensed_dataset_reconstructed)
+      lensed_coeff_dataset=np.array(lensed_coeff_dataset)
+    
+      #noisy_dataset_reconstructed=np.array(noisy_dataset_reconstructed)
+      noisy_coeff_dataset=np.array(noisy_coeff_dataset)
+      
+      
+      dataFrame_unlensed_coeff_dataset = pd.DataFrame(unlensed_coeff_dataset)
+      dataFrame_unlensed_coeff_dataset.columns = np.linspace(0,len(coeff_ngc)-1,num=len(coeff_ngc),dtype=int)
+      dataFrame_unlensed_coeff_dataset.to_csv(source_reconstruction+'/DataFrame_unlensed_coeff_dataset/Unlensed_coeff_dataset_DataFrame.csv')
+      
+      dataFrame_lensed_coeff_dataset = pd.DataFrame(lensed_coeff_dataset)
+      dataFrame_lensed_coeff_dataset.columns = np.linspace(0,len(coeff_ngc)-1,num=len(coeff_ngc),dtype=int)
+      dataFrame_lensed_coeff_dataset.to_csv(source_reconstruction+'/DataFrame_lensed_coeff_dataset/Lensed_coeff_dataset_DataFrame.csv')
+      
+      dataFrame_noisy_coeff_dataset = pd.DataFrame(noisy_coeff_dataset)
+      dataFrame_noisy_coeff_dataset.columns = np.linspace(0,len(coeff_ngc)-1,num=len(coeff_ngc),dtype=int)
+      dataFrame_noisy_coeff_dataset.to_csv(source_reconstruction+'/DataFrame_noisy_coeff_dataset/Noisy_coeff_dataset_DataFrame.csv')
+      
+      
+    
+source_dataset='C:/Users/Ayoub/Desktop/PFE/Generation_Dataset/Dataset'
 
-  
-    image_1d = util.image2array(dataset[ind][1])  
-    # decompose image and return the shapelet coefficients
-    coeff_ngc = shapeletSet.decomposition(image_1d, x, y, nmax, beta, deltaPix=1, center_x=0, center_y=0) 
-    # reconstruct NGC1300 with the shapelet coefficients
-    image_reconstructed = shapeletSet.function(x, y, coeff_ngc, nmax, beta, center_x=0, center_y=0)
-    # turn 1d array back into 2d image
-    image_reconstructed_2d = util.array2image(image_reconstructed)  # map 1d data vector in 2d image
-    dataset_reconstructed_local.append(image_reconstructed_2d)
-    coeff_dataset_local.append(coeff_ngc)
+if not os.path.exists(source_dataset):
+    generating_dataset(n,source_dataset, data_class, psf_class, kwargs_numerics, exp_time, background_rms, x, y)#generating_dataset(n, data_class, psf_class, kwargs_numerics, exp_time, background_rms, x, y)#generating_dataset(n,data_class,psf_class,kwargs_numerics,exp_time,background_rms,x,y)
 
 
-    image_1d = util.image2array(dataset[ind][2])  
-    # decompose image and return the shapelet coefficients
-    coeff_ngc = shapeletSet.decomposition(image_1d, x, y, nmax, beta, deltaPix=1, center_x=0, center_y=0) 
-    # reconstruct NGC1300 with the shapelet coefficients
-    image_reconstructed = shapeletSet.function(x, y, coeff_ngc, nmax, beta, center_x=0, center_y=0)
-    # turn 1d array back into 2d image
-    image_reconstructed_2d = util.array2image(image_reconstructed)  # map 1d data vector in 2d image
-    dataset_reconstructed_local.append(image_reconstructed_2d)
-    coeff_dataset_local.append(coeff_ngc)
-
-    dataset_reconstructed.append(np.array(dataset_reconstructed_local))
-    coeff_dataset.append(np.array(coeff_dataset_local))
-  
-  dataset_reconstructed=np.array(dataset_reconstructed)
-  coeff_dataset=np.array(coeff_dataset)
-
-  return(dataset_reconstructed,coeff_dataset)
-
-
+source_reconstruction='C:/Users/Ayoub/Desktop/PFE/Generation_Dataset/Shapelet_dataset'
 n_=n
 choice_type_shapelet=ShapeletSet()
-dataset_=dataset
 
-numPix=128
-nmax=170
 
-theta_max=numPix
-theta_min=theta_max/(nmax+1)
+
+theta_min=0.75
+theta_max=128
 
 beta=np.sqrt(theta_max*theta_min)
+nmax=int(theta_max/theta_min-1)
 
-deltaPix=theta_min
 print('\nbeta = '+str(beta))
 print('nmax = '+str(nmax))
 
-x, y = util.make_grid(numPix, deltaPix)
 
-dataset_reconstructed,coeff_dataset=shapelet_reconstruction(n_,choice_type_shapelet,dataset_,beta,nmax,x,y,deltaPix)
+x,y=util.make_grid(numPix=theta_max,deltapix=theta_min)
+delta=1
+
+
+shapelet_reconstruction(n,source_dataset,source_reconstruction,choice_type_shapelet,beta,nmax,x,y,deltaPix)
