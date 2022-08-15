0747a235473f665b218d9d1c6afa212685d9cbad
tojoub <63126897+tojoub@users.noreply.github.com>
2022-08-15 12:39:41 +0200

    Add files via upload

---
+++ network_unlensed_images.py
@@ -0,0 +1,123 @@
+from import_and_installations import *
+
+import tensorflow as tf
+from tensorflow.keras import datasets, layers, models
+from keras.models import Model, load_model
+from keras.layers import Input,BatchNormalization,Activation
+from keras.layers.core import Dropout, Lambda
+from keras.layers.convolutional import Conv2D, Conv2DTranspose
+from keras.layers.pooling import MaxPooling2D
+from keras.layers.merge import concatenate
+from keras.callbacks import EarlyStopping, ModelCheckpoint
+from keras import backend as K
+
+
+def compare_images(target, ref):
+    target = np.squeeze(target/255)
+    ref = np.squeeze(ref/255)
+    scores = []
+    scores.append(psnr(target, ref))
+    scores.append(mse(target, ref))
+    scores.append(ssim(target, ref, multichannel=True))
+    return scores
+# Loss functtion
+def ssim_loss(y_true, y_pred):
+  return 1 - tf.reduce_mean(tf.image.ssim(y_true, y_pred, 1.0))
+def psnr_loss(y_true, y_pred):
+  return tf.image.psnr(y_true, y_pred, 1.0)
+def mse_ssim_loss(y_true, y_pred):
+  return tf.reduce_mean(tf.math.squared_difference(y_true, y_pred)) - tf.reduce_mean(tf.image.ssim(y_true, y_pred, 1.0))
+
+
+inputs = Input((128, 128, 1))
+s = Lambda(lambda x: x / 255) (inputs)
+
+c1 = Conv2D(16, (3, 3),  padding='same',use_bias=False) (s)
+c1 = BatchNormalization()(c1)
+c1 = Activation('relu')(c1)
+c1 = Conv2D(16, (3, 3), padding='same',use_bias=False) (c1)
+c1 = BatchNormalization()(c1)
+c1 = Activation('relu')(c1)
+p1 = MaxPooling2D((2, 2)) (c1)
+
+c2 = Conv2D(32, (3, 3),  padding='same',use_bias=False) (p1)
+c2 = BatchNormalization()(c2)
+c2 = Activation('relu')(c2)
+c2 = Conv2D(32, (3, 3), padding='same',use_bias=False) (c2)
+c2 = BatchNormalization()(c2)
+c2 = Activation('relu')(c2)
+p2 = MaxPooling2D((2, 2)) (c2)
+
+c3 = Conv2D(64, (3, 3), padding='same',use_bias=False) (p2)
+c3 = BatchNormalization()(c3)
+c3 = Activation('relu')(c3)
+c3 = Conv2D(64, (3, 3), padding='same',use_bias=False) (c3)
+c3 = BatchNormalization()(c3)
+c3 = Activation('relu')(c3)
+c3 = Conv2D(64, (3, 3), padding='same',use_bias=False) (c3)
+c3 = BatchNormalization()(c3)
+c3 = Activation('relu')(c3)
+p3 = MaxPooling2D((2, 2)) (c3)
+
+c4 = Conv2D(128, (3, 3), padding='same', use_bias=False) (p3)
+c4 = BatchNormalization()(c4)
+c4 = Activation('relu')(c4)
+c4 = Conv2D(128, (3, 3), padding='same',use_bias=False) (c4)
+c4 = BatchNormalization()(c4)
+c4 = Activation('relu')(c4)
+p4 = MaxPooling2D(pool_size=(2, 2)) (c4)
+
+c5 = Conv2D(256, (3, 3), padding='same',use_bias=False) (p4)
+c5 = BatchNormalization()(c5)
+c5 = Activation('relu')(c5)
+c5 = Conv2D(256, (3, 3),  padding='same',use_bias=False) (c5)
+c5 = BatchNormalization()(c5)
+c5 = Activation('relu')(c5)
+
+u6 = Conv2DTranspose(128, (2, 2), strides=(2, 2), padding='same') (c5)
+u6 = concatenate([u6, c4])
+c6 = Conv2D(128, (3, 3),  padding='same',use_bias=False) (u6)
+c6 = BatchNormalization()(c6)
+c6 = Activation('relu')(c6)
+c6 = Conv2D(128, (3, 3),  padding='same',use_bias=False) (c6)
+c6 = BatchNormalization()(c6)
+c6 = Activation('relu')(c6)
+
+u7 = Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='same') (c6)
+u7 = concatenate([u7, c3])
+c7 = Conv2D(64, (3, 3),  padding='same',use_bias=False) (u7)
+c7 = BatchNormalization()(c7)
+c7 = Activation('relu')(c7)
+c7 = Conv2D(64, (3, 3),  padding='same',use_bias=False) (c7)
+c7 = BatchNormalization()(c7)
+c7 = Activation('relu')(c7)
+c7 = Conv2D(64, (3, 3),  padding='same',use_bias=False) (c7)
+c7 = BatchNormalization()(c7)
+c7 = Activation('relu')(c7)
+
+u8 = Conv2DTranspose(32, (2, 2), strides=(2, 2), padding='same') (c7)
+u8 = concatenate([u8, c2])
+c8 = Conv2D(32, (3, 3), padding='same',use_bias=False) (u8)
+c8 = BatchNormalization()(c8)
+c8 = Activation('relu')(c8)
+c8 = Conv2D(32, (3, 3), padding='same',use_bias=False) (c8)
+c8 = BatchNormalization()(c8)
+c8 = Activation('relu')(c8)
+
+u9 = Conv2DTranspose(16, (2, 2), strides=(2, 2), padding='same') (c8)
+u9 = concatenate([u9, c1], axis=3)
+c9 = Conv2D(16, (3, 3),  padding='same',use_bias=False) (u9)
+c9 = BatchNormalization()(c9)
+c9 = Activation('relu')(c9)
+c9 = Conv2D(16, (3, 3), padding='same',use_bias=False) (c9)
+c9 = BatchNormalization()(c9)
+c9 = Activation('relu')(c9)
+
+out = Conv2D(1, (1, 1), use_bias=False) (c9)
+out = BatchNormalization()(out)
+outputs = Activation('linear')(out)
+# Optimizer
+optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
+
+model2 = Model(inputs=[inputs], outputs=[outputs])
+
