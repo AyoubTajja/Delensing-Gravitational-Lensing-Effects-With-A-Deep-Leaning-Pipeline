3ffe1222ff4fa3d9f09ff5716a4401c6f8395332
tojoub <63126897+tojoub@users.noreply.github.com>
2022-08-18 15:12:50 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,44 +1,72 @@
 from import_and_installations import *
 from open_dataset import *
 from network_lens_parameters import model1
-#from network_unlensed_images import model2
-from Dataset_generation import generating_dataset,x,y,numPix,deltaPix,data_class, psf_class, kwargs_numerics, exp_time, background_rms,source_dataset,n
+from network_unlensed_images import model2
+from Dataset_generation import generating_dataset,x,y,numPix,deltaPix,data_class, psf_class, kwargs_numerics, exp_time, background_rms,folder_path_dataset,n
+from Shapelet_Reconstruction import folder_path_reconstruction,shapelet_reconstruction,choice_type_shapelet,beta,nmax,delta
 
 
-if not os.path.exists(source_dataset):
-    generating_dataset(n,source_dataset, data_class, psf_class, kwargs_numerics, exp_time, background_rms, x, y)
+if not os.path.exists(folder_path_dataset):
+    generating_dataset(n,folder_path_dataset, data_class, psf_class, kwargs_numerics, exp_time, background_rms, x, y)
 
-## Open dataset
-# unlensed_dataset is a numpy.array type
-folder='Unlensed_Dataset/'
-unlensed_dataset=open_dataset(source_dataset+'/'+folder)
-# lensed_dataset is a numpy.array type
-folder='Lensed_Dataset/'
-lensed_dataset=open_dataset(source_dataset+'/'+folder)
-# Noisy_dataset is a numpy.array type
-folder='Noisy_Dataset/'
-Noisy_Dataset_dataset=open_dataset(source_dataset+'/'+folder)
+## Image Dataset
+filename='unlensed_dataset.npy'
+unlensed_dataset=np.load(folder_path_dataset+'/'+filename)
+
+filename='lensed_dataset.npy'
+lensed_dataset=np.load(folder_path_dataset+'/'+filename)
+
+filename='noisy_dataset.npy'
+noisy_dataset=np.load(folder_path_dataset+'/'+filename)
+
+## Label Datasets
+filename='lens_parameters.npy'
+lens_parameters_dataset=np.load(folder_path_dataset+'/'+filename)
+
+filename='source_types.npy'
+source_type_dataset=np.load(folder_path_dataset+'/'+filename,allow_pickle=True)
+
+filename='source_parameters.npy'
+source_parameters_dataset=np.load(folder_path_dataset+'/'+filename,allow_pickle=True)
 
 
-folder='DataFrame_lens_parameters/'
-filename = os.listdir(source_dataset+'/'+folder)[0]
-lens_parameters_df = pd.read_csv(source_dataset+'/'+folder+filename)
+if not os.path.exists(folder_path_reconstruction):
+    shapelet_reconstruction(n,folder_path_dataset,folder_path_reconstruction,choice_type_shapelet,beta,nmax,x,y,delta)
 
-lensed_parameters_dataset=lens_parameters_df.to_numpy()
-lensed_parameters_dataset=np.transpose(lensed_parameters_dataset)[1:]
-lensed_parameters_dataset=np.transpose(lensed_parameters_dataset)
 
+# Shapelet Reconstruction Datasets
+## Image Dataset
+filename='unlensed_dataset_reconstructed_images.npy'
+unlensed_dataset_reconstructed=np.load(folder_path_reconstruction+'/'+filename)
+
+filename='lensed_dataset_reconstructed_images.npy'
+lensed_dataset_reconstructed=np.load(folder_path_reconstruction+'/'+filename)
+
+filename='noisy_dataset_reconstructed_images.npy'
+noisy_dataset_reconstructed=np.load(folder_path_reconstruction+'/'+filename)
+## Shapelet Coeff Dataset
+filename='unlensed_dataset_reconstructed_coeff.npy'
+unlensed_dataset_reconstructed_coeff=np.load(folder_path_reconstruction+'/'+filename)
+
+filename='lensed_dataset_reconstructed_coeff.npy'
+lensed_dataset_reconstructed_coeff=np.load(folder_path_reconstruction+'/'+filename)
+
+filename='noisy_dataset_reconstructed_coeff.npy'
+noisy_dataset_reconstructed_coeff=np.load(folder_path_reconstruction+'/'+filename)
+
+
+
+
+n=len(lensed_dataset)
 
 ## Define the set of training,validation,test
 
 x_train = lensed_dataset[0:int(0.75*n)].reshape(-1,128, 128, 1)
-trainLabels=lensed_parameters_dataset[0:int(0.75*n)]
+trainLabels=lens_parameters_dataset[0:int(0.75*n)]
 x_dev=lensed_dataset[int(0.75*n):int(0.9*n)].reshape(-1,128, 128, 1)
-devLabels=lensed_parameters_dataset[int(0.75*n):int(0.9*n)]
+devLabels=lens_parameters_dataset[int(0.75*n):int(0.9*n)]
 x_test=lensed_dataset[int(0.9*n):].reshape(-1,128, 128, 1)
-testLabels=lensed_parameters_dataset[int(0.9*n):]
-
-
+testLabels=lens_parameters_dataset[int(0.9*n):]
 
 #####################################################################################################
 #####################################################################################################
@@ -49,7 +77,7 @@ testLabels=lensed_parameters_dataset[int(0.9*n):]
 model1.summary()
 
 model1.compile(loss='mean_squared_error', optimizer='adam', metrics=['mse', 'mae', 'mape'])
-history = model1.fit(x_train, trainLabels,batch_size=128, epochs=7, validation_data=(x_dev, devLabels))
+history = model1.fit(x_train, trainLabels,batch_size=128, epochs=100, validation_data=(x_dev, devLabels))
 
 
 # Get training and test loss histories
@@ -87,7 +115,10 @@ testout = pd.DataFrame(model1.predict(x_test))
 testout.columns = ['theta_E_out', 'gamma_out', 'center_x_out', 'center_y_out','e1_out', 'e2_out']
 display(testout)
 
-display(lens_parameters_df)
+
+testLabels_df=pd.DataFrame(testLabels)
+testLabels_df.columns = ['theta_E', 'gamma', 'center_x', 'center_y','e1', 'e2']
+display(testLabels_df)
 
 
 #####################################################################################################
@@ -96,8 +127,16 @@ display(lens_parameters_df)
 ## First DeepLearning Model to find the unlensed images
 ## The input : the Lensed images
 ## The output : the unlensed images
+
+
+x_train_unlensed = unlensed_dataset[0:int(0.75*n)].reshape(-1,128, 128, 1)
+x_dev_unlensed=unlensed_dataset[int(0.75*n):int(0.9*n)].reshape(-1,128, 128, 1)
+x_test_unlensed=unlensed_dataset[int(0.9*n):].reshape(-1,128, 128, 1)
+
+
+
 model2.summary()
-tf.keras.utils.plot_model(model6, show_shapes=True)
+tf.keras.utils.plot_model(model2, show_shapes=True)
 
 model2.compile(loss=mse_ssim_loss, optimizer=optimizer, metrics=['mse',ssim_loss,psnr_loss])
 history2 = model2.fit(x_train, x_trainunlensed,batch_size=128, epochs=  10, validation_data=(x_dev, x_devunlensed))
